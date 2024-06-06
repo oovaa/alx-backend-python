b5f0a3e75d24db35cca441b0e98a5fade15157d8
oovaa <omarvx211@gmail.com>
2024-06-06 12:16:59 +0200

    hotfix at 06-12

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
-'''
-task 2
-'''
-
+""" Basic annotations concat """
 
 import math
 
 
-def floor(n: float) -> float:
-    '''
-    type-annotated function concat that takes a string str1 and a string str2
-    as arguments and returns a concatenated string
-    '''
+def floor(n: float) -> int:
+    """ Returns the floor of the float """
     return math.floor(n)
--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,18 +1,7 @@
 #!/usr/bin/env python3
-'''
-task 3
-'''
+""" Basic annotations - to string  """
 
 
 def to_str(n: float) -> str:
-    """
-    Convert a float to a string representation.
-
-    Args:
-        n (float): The float number to be converted.
-
-    Returns:
-        str: The string representation of the float.
-
-    """
-    str(n)
+    """ Returns the string representation of the float. """
+    return str(n)
