58e0841f24c1c50cc9c04c51597f054a99b51984
oovaa <omarvx211@gmail.com>
2024-06-06 12:24:33 +0200

    task 5 at 06-12

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''
+task 5
+'''
+
+
+def sum_list(input_list: list) -> float:
+    """
+    Calculate the sum of all the elements in the input list.
+
+    Args:
+        input_list (list): A list of numbers.
+
+    Returns:
+        float: The sum of all the elements in the input list.
+    """
+    return sum(input_list)
+
+
+def sum_list(input_list: list) -> float:
+    return sum(list)
