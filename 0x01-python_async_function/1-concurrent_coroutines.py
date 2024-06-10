756cfb5c777e75ab227078dfbc13b4018b8abc04
oovaa <omarvx211@gmail.com>
2024-06-10 20:58:25 +0200

    add types at 10-20

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,12 +6,13 @@ task 1
 
 
 import asyncio
+from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """wait_n should return the list of all the delays (float values).
     The list of the delays should be in ascending order without using
     sort() because of concurrency."""
